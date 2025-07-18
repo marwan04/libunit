3ce010f0300ab62ec4cb295d6f58341598a884a0
Moe-Salim91156 <hmodaisleem88@gmail.com>
2025-07-18 23:17:05 +0300

    working

--- real-tests/01-basic-test.c
+++ real-tests/01-basic-test.c
@@ -6,13 +6,14 @@
 /*   By: moe <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 21:43:31 by moe               #+#    #+#             */
-/*   Updated: 2025/07/18 22:33:04 by moe              ###   ########.fr       */
+/*   Updated: 2025/07/18 23:16:37 by moe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include	"tests.h"
 
 int basic_line_test(void) {
 	int fd = open("testfiles/basic.txt", O_RDONLY);
+	printf("%d\n",fd);
 	if (fd < 0)
 		return (-1);
 	char *line = get_next_line(fd);

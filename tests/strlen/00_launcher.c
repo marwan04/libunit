f42af23bf99fdfcca485d5a9e1e5e4896f3f2a3a
Marwan Alrifai <marwanalrfai5@gmail.com>
2025-07-19 20:38:11 +0300

    Seg handle

--- tests/strlen/00_launcher.c
+++ tests/strlen/00_launcher.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   00_launcher.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moe <marvin@42.fr>                         +#+  +:+       +#+        */
+/*   By: alrfa3i <alrfa3i@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 20:25:02 by moe               #+#    #+#             */
-/*   Updated: 2025/07/19 20:25:03 by moe              ###   ########.fr       */
+/*   Updated: 2025/07/19 20:36:24 by alrfa3i          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,15 @@ int	bus_test(void);
 int	strlen_launcher(void)
 {
 	t_unit_test	*list;
+	int			result;
 
 	list = NULL;
 	load_test(&list, "OK test", &ok_test);
 	load_test(&list, "KO test", &ko_test);
 	load_test(&list, "SEGV test", &segv_test);
 	load_test(&list, "BUS test", &bus_test);
-	cleanup_tests(list);
-	return (launch_tests(&list));
+	result = launch_tests(&list);
+	if (list)
+		cleanup_tests(list);
+	return (result);
 }
